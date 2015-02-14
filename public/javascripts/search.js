6d581d1f2f42a0211f8162db1a0ae1dc65aa3f1b
Brent Williams <brent.robert.williams@gmail.com>
2015-02-14 12:59:09 -0800

    Got google map in and search column

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
    setContentSize();
+   initializeMap();
 });
 
 function setContentSize () {
@@ -7,10 +8,23 @@ function setContentSize () {
    var width = $(window).width();
    $("#content").css("height", (height - 100).toString() + "px");
    $("#content").css("width", width.toString() + "px");
+
+   $("#map-canvas").css("width", (width-400).toString() + "px");
 }
 
 function resize (argument) {
    setContentSize();
 }
+
+function initializeMap () {
+   var mapCanvas = document.getElementById('map-canvas');
+   var mapOptions = {
+      center: new google.maps.LatLng(35.282752, -120.659616),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+   }
+   var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
 
 $(window).resize(resize);
