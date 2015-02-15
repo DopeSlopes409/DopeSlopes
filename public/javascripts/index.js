1b4e52542e867ff7d41de8c26ae8d843a57ce136
Alex Cross <abcross@calpoly.edu>
2015-02-15 15:03:06 -0800

    Added geolocation error handling

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -46,9 +46,22 @@ $("#use-my-loc").click(function() {
             //navigate to the url + uri
          },
          //make serperate error funct. 
-         function (error) {
-            alert('Location Error');
-         });
+         function (error){
+            switch(error.code) {
+                  case error.PERMISSION_DENIED:
+                      alert("User denied the request for Geolocation.");
+                      break;
+                  case error.POSITION_UNAVAILABLE:
+                      alert("Location information is unavailable.");
+                      break;
+                  case error.TIMEOUT:
+                      alert("The request to get user location timed out.");
+                      break;
+                  case error.UNKNOWN_ERROR:
+                      alert("An unknown error occurred.");
+                      break;
+            }
+        });
     } else { 
         alert("Geolocation is not supported by this browser.");
     };
