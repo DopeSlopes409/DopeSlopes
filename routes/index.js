0bd4a12a8f5bfeda118a17cc6588d9fe48843272
Victor Zellweger <vazellweg@gmail.com>
2015-02-16 15:36:36 -0800

    Fix API request issues caused from testing on front-end calls. Render index page in case of API error.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -22,7 +24,7 @@ router.get('/location_search/:query', function (req, res, next) {
 
 
    //  Figure out how to get GMaps to render location on client-side
-   // with geolocation as a dust variable.
+   // with geolocation as a dust variable.b
 
    
    //
@@ -33,55 +35,86 @@ router.get('/location_search/:query', function (req, res, next) {
          {css: 'search.css'}],
       javascriptFiles: [{javascript: 'search.js'}]
    };
-   res.render('resort_search', dustVars);
+   res.render('search2', dustVars);
 });
 
 
 /* GET request for searching resorts based on geolocation */
-router.get('/resort_search/:geolocation', function (req, res, next) 
+router.get('/resort_search', function (req, res, next) 
 {
+  var latitude = req.query.latitude;
+  var longitude = req.query.longitude;
+  var state = req.query.state;
+  
+  console.log('latitude: ' + latitude + ', longitude: ' + longitude + ', state: ' + state);
   // Request Snocountry API for southwest region
   // var reqResults; //repsonse from API  
-  var dustVars = {
-    title: 'DopeSlopes',
-    cssFiles: [{css: 'index.css'},
-     {css: 'search.css'}],
-    resorts : []
-  };
+ 
+  // TODO: get region for supplied state arg
+  
+  var region = "northwest";
+  var url = "http://feeds.snocountry.net/conditions.php?regions="+ region +"&apiKey=api8932.test5643&output=json";
+  var numItems;
 
-  //for each result resort (elt)
-  /*
-  var REGION = "northwest";
-  var url = "http://feeds.snocountry.net/conditions.php?regions="+REGION+"&apiKey=api8932.test5643&output=json";
-  var id, name, latitude, longitude, open_runs, total_runs, recent_snowfall, numItems;
-
-  $.ajax({
+  request.get({
       url: url,
-      dataType: 'json',
-      success: function(response) {
-          //numItems = response.totalItems;
-          numItems = 5;   //  remove once testing finished
-          for(var i = 0; i < numItems; i++) {
-              id = response.items[i].id;
-            dustVars.resorts.push({
-              name = response.items[i].resortName;
-              latitude = response.items[i].latitude;
-              longitude = response.items[i].longitude;
-              open_runs = response.items[i].openDownHillTrails;
-              total_runs = response.items[i].maxOpenDownHillTrails;
-              recent_snowfall = (response.items[i].newSnowMax + response.items[i].newSnowMin) / 2;
-            });
-              window.alert ("resort: " + name + " lat: " + latitude + " lon: " + longitude + " open_runs: " + open_runs 
-                  + " total_runs: " + total_runs + " snow: " + recent_snowfall)
-          }        
-      },
-      error : function() { window.alert("error"); }
- });
+      json: true},
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log(body.items[0]);
+          
+            //numItems = response.totalItems;
+            numItems = 5;   //  remove once testing finished
 
+            var dustVars = {
+              title: 'DopeSlopes',
+              cssFiles: [{css: 'search2.css'}],
+              javascriptFiles: [{javascript: 'search2.js'}],
+              resortEntries : []
+            };
+
+
+            dustVars.resortEntries = body.items.map(function (entry) {
+              return {id: entry.id,
+                name: entry.resortName,
+                latitude: entry.latitude,
+                longitude: entry.longitude,
+                openRuns: entry.openDownHillTrails,
+                totalRuns: entry.maxOpenDownHillTrails,
+                recentSnowfall: (entry.newSnowMax + entry.newSnowMin) / 2};
+            });
+
+            //TODO: Sort resortEntries by travel time from current geolocation
+
+            //truncate resortEntries length 
+            dustVars.resortEntries.splice(numItems);
+            
+            dustVars.resortEntries.forEach(function (elt) {
+              console.log("resort: " + elt.name + " lat: " + elt.latitude + " lon: " + elt.longitude + " open_runs: " + elt.open_runs 
+                    + " total_runs: " + elt.total_runs + " snow: " + elt.recent_snowfall);
+            }); 
+
+            res.render('search2', dustVars);        
+          }
+          else {
+            console.log("Unable to access REST API.");
+            console.log("Error: " + error);
+
+            var dustVars = {
+              title: 'DopeSlopes',
+              cssFiles: [{css: 'index.css'}],
+              javascriptFiles: [{javascript: 'index.js'}],
+              resortEntries : []
+            };
+
+            res.render('index', dustVars);
+          }
+        });
+
+  console.log('left ajax call');
   //render results
-  res.render('resort_search', dustVars);
-}
-  */
+  //res.render('resort_search', dustVars);
+
 });
 
 /** 
@@ -104,7 +137,6 @@ router.get('/search', function(req, res, next) {
  * This is just temporary so I (Brent) can work on a second version of the front end.
  */
 router.get('/search2', function(req, res, next) {
-
 
    var dustVars = {
       title: 'Search2',
