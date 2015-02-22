295251c79bdc26fd66ec4839ce15f195ac6dcf82
Victor Zellweger <vazellweg@gmail.com>
2015-02-22 14:27:18 -0800

    Simple Google signin
    Simple google signin for index page. Will eventually move all signin and
    navbar logic to seperate files to work on all pages on site.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret: 'ssshhhhturnandburnssshhh'}));
+//app.use(session({secret: 'ssshhhhturnandburnssshhh'}));
 
 //serve public resources
 app.use(express.static(path.join(__dirname, 'public')));
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	initializeClickListeners();
-
+  $('#popup').hide();
   //hide something about the background
 
   $('#background_cycler').removeClass('hide');
@@ -11,6 +11,13 @@ $(document).ready(function() {
    // some geolaction function for defining region
 
    //initializeAutoComplete();
+   //init login/logout links
+   $('#login-link').click(function() {
+    $("#popup").show();
+   });
+
+//   
+   //$('#no-alarms').hide();
    setPrompt();
 });
 
@@ -25,6 +32,14 @@ function cycleImages() {
   });
 }
 
+
+
+
+
+// Hide the login prompt
+function showLoginPopup() {
+  $("#popup").hide();
+}
 
 // set search prompt to middle of page
 function setPrompt() {
@@ -132,6 +147,47 @@ function initializeAutoComplete() {
    $('#basic_search_input').autocomplete(
       //fields
       );   
+}
+
+/**
+* Handler for the signin callback triggered after the user selects an account.
+*/
+function onSignInCallback(resp) {
+   gapi.client.load('plus', 'v1', apiClientLoaded);
+}
+
+/**
+* Sets up an API call after the Google API client loads.
+*/
+function apiClientLoaded() {
+   gapi.client.plus.people.get({userId: 'me'}).execute(handleEmailResponse);
+}
+
+/**
+* Response callback for when the API client receives a response.
+*
+* @param resp The API response object with the user email and profile information.
+*/
+function handleEmailResponse(resp) {
+   var primaryEmail;
+
+   if (resp.code === 403) {
+      return;
+   }
+   console.log('email response: ' + JSON.stringify(resp));
+   $('#gConnect').hide();
+   for (var i=0; i < resp.emails.length; i++) {
+     if (resp.emails[i].type === 'account') primaryEmail = resp.emails[i].value;
+   }
+
+   //hide login links
+   $('#login').hide();
+   $('#session').toggleClass('hide');
+   $('#session #username').text('user: ' + primaryEmail);
+        // + '\n\nFull Response:\n' + JSON.stringify(resp);
+    //hide popup
+    $('#popup').hide();
+   //load session data for logged-in user
 }
 
 
